7b7f3f6a520f701a0a732d87b00e9b0cfda910b3
Justin Falcone <justin@justinfalcone.com>
2018-12-01 12:19:15 -0500

    include both answers

--- day-1/program.hs
+++ day-1/program.hs
@@ -5,9 +5,10 @@ import qualified Data.Set     as Set
 main = do
     ls <- fmap Text.lines $ Text.readFile "input-1.txt"
     let ints = fmap (toInt . Text.unpack) ls
+    let part1 = sum ints
     let loop = ints ++ loop
-    let value = firstDuplicate loop 0 Set.empty
-    return value
+    let part2 = firstDuplicate loop 0 Set.empty
+    return (part1, part2)
 
 toInt :: String -> Int
 toInt ('+' : num) = read num

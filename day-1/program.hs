acf6b61cedaa62d5a4b316b1bc60ef9a0e5db992
Justin Falcone <justin@justinfalcone.com>
2018-12-02 13:21:43 -0500

    foldlM

--- day-1/program.hs
+++ day-1/program.hs
@@ -1,12 +1,13 @@
 import qualified Data.IntSet  as Set
-import Data.Foldable (foldrM)
+import Data.Foldable (foldlM)
+import Data.Either (fromLeft)
 
 main = do
-    ls <- fmap lines $ readFile "input-1.txt"
+    ls <- lines <$> readFile "input-1.txt"
     let ints = fmap toInt ls
     let part1 = sum ints
     let loop = ints ++ loop
-    let (part2, _) = foldUntil findDup initState loop
+    let part2 = fromLeft 0 $ foldlM findDup initState loop
     return (part1, part2)
 
 toInt :: String -> Int
@@ -15,14 +16,8 @@ toInt num = read num
 
 initState = (0, Set.empty)
 
-foldUntil :: (a -> b -> Maybe b) -> b -> [a] -> b
-foldUntil _ state [] = state
-foldUntil f state (x : xs) =
-    case f x state of
-        Just next -> foldUntil f next xs
-        Nothing -> state
-
-findDup x (sum, visited) =
-    if Set.member sum visited
-    then Nothing
-    else Just (sum + x, Set.insert sum visited)
+findDup (sum, visited) x =
+    let next = sum + x in
+    if Set.member next visited
+    then Left next
+    else Right (next, Set.insert next visited)
